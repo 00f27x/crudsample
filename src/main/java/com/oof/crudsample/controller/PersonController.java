5f11026b34220f7e2fddf3689a769dd5f266cd1c
ivan <ivanhfc00@gmail.com>
2022-05-31 18:33:50 -0300

    Criada página de adição de pessoa

--- src/main/java/com/oof/crudsample/controller/PersonController.java
+++ src/main/java/com/oof/crudsample/controller/PersonController.java
@@ -1,10 +1,12 @@
 package com.oof.crudsample.controller;
 
+import com.oof.crudsample.domain.Person;
 import com.oof.crudsample.repository.PersonRepository;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class PersonController {
@@ -19,5 +21,11 @@ public class PersonController {
     public String listAll(Model model){
         model.addAttribute("listPersons", this.personRepository.findAll());
         return "hr/persons/index";
+    }
+
+    @GetMapping("/hr/persons/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "hr/persons/form";
+
     }
 }
